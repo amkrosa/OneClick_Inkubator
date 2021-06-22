bab2e9cbaf86e38faf5dacecd6003c19d82bb7f1
Anna Krasowska <annakrasowska2@gmail.com>
2021-06-22 19:46:26 +0200

    fill methods for PaymentForPage

--- src/main/java/Pages/PaymentFormPage.java
+++ src/main/java/Pages/PaymentFormPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
-public class PaymentFormPage {
+public class PaymentFormPage extends BasePage{
 
     @FindBy(how = How.ID, using = "data-processing-agreement")
     WebElement dataProcessingAgreementCheckbox;
@@ -15,6 +15,26 @@ public class PaymentFormPage {
     @FindBy(how = How.XPATH, using = "//*[@class='finish-button']")
     WebElement finishButton;
 
+    public PaymentFormPage(){super();}
 
+    public PaymentFormPage fillEmailField(String text) {
+        emailField.sendKeys(text);
+        return this;
+    }
 
+    public PaymentFormPage clickDataProcessingAgreementCheckbox() {
+        if (!dataProcessingAgreementCheckbox.getAttribute("checked").equals("checked"))
+            getCommonHelper().moveAndClick(dataProcessingAgreementCheckbox);
+        return this;
+    }
+
+    public PaymentFormPage clickMtransferPaymentButton() {
+        getCommonHelper().moveAndClick(mtransferPaymentButton);
+        return this;
+    }
+
+    public PaymentFormPage clickFinishButton() {
+        getCommonHelper().moveAndClick(finishButton);
+        return this;
+    }
 }
