bece285c3fba7a11e19852287eecda4249453c8b
Anna Krasowska <annakrasowska2@gmail.com>
2021-06-22 19:22:45 +0200

    refactor BoxmachineFormPage for BasePage

--- src/main/java/Pages/BoxmachineFormPage.java
+++ src/main/java/Pages/BoxmachineFormPage.java
@@ -9,33 +9,19 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
-public class BoxmachineFormPage extends FormPage {
-
-    private WebDriver driver;
-    private CommonHelper commonHelper;
-    private WaitHelper waitHelper;
-    private ActionHelper actionHelper;
+public class BoxmachineFormPage extends BasePage {
 
     @FindBy(how = How.CSS, using = ".ng-input > input")
     WebElement parcelmachine;
     @FindBy(how = How.CSS, using = "app-geowidget")
     WebElement geowidget;
 
-    BoxmachineFormPage(){  super(); }
+    public BoxmachineFormPage(){  super(); }
 
     public BoxmachineFormPage setParcelmachine(String pm) {
         parcelmachine.sendKeys(pm);
-        waitHelper.waitUntilClickable(By.cssSelector("#" + pm.toUpperCase()));
-        driver.findElement(By.cssSelector("#" + pm.toUpperCase())).click();
+        getWaitHelper().waitUntilClickable(By.cssSelector("#" + pm.toUpperCase()));
+        getDriver().findElement(By.cssSelector("#" + pm.toUpperCase())).click();
         return this;
     }
-
-    public BoxmachineFormPage fillReceiverParcelmachine(String name, String phone, String email, String parcelmachine) {
-        super.receiverName.sendKeys(name);
-        receiverEmail.sendKeys(email);
-        receiverPhone.sendKeys(phone);
-        setParcelmachine(parcelmachine);
-        return this;
-    }
-
 }
