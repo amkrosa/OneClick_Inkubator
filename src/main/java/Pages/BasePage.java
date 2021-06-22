48787614294a2a2ac0e33e96f7d6c5b72f4251c2
Anna Krasowska <annakrasowska2@gmail.com>
2021-06-22 19:08:58 +0200

    abstract BasePage class as base for other pages

---
+++ src/main/java/Pages/BasePage.java
@@ -0,0 +1,37 @@
+package Pages;
+
+import Helpers.ActionHelper;
+import Helpers.CommonHelper;
+import Helpers.WaitHelper;
+import SeleniumBase.Base;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public abstract class BasePage {
+    private WebDriver driver;
+    private CommonHelper commonHelper;
+    private WaitHelper waitHelper;
+    private ActionHelper actionHelper;
+
+    public BasePage(){
+        driver = Base.driver;
+        PageFactory.initElements(driver, this);
+        waitHelper = new WaitHelper(new WebDriverWait(Base.driver, 3));
+        actionHelper = new ActionHelper(new Actions(Base.driver));
+        commonHelper = new CommonHelper(actionHelper, waitHelper);
+    }
+
+    public CommonHelper getCommonHelper() {
+        return commonHelper;
+    }
+
+    public WaitHelper getWaitHelper() {
+        return waitHelper;
+    }
+
+    public ActionHelper getActionHelper() {
+        return actionHelper;
+    }
+}
