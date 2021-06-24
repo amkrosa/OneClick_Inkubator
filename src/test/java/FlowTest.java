f697e1b5ab0ffc8cfba2f74c800e2885b9e38cf4
Anna Krasowska <annakrasowska2@gmail.com>
2021-06-24 21:40:21 +0200

    Added tests up to final summary page for parcelmachine delivery method - without real label download or print label button testing

--- src/test/java/FlowTest.java
+++ src/test/java/FlowTest.java
@@ -2,10 +2,7 @@ import Configs.EnvironmentType;
 import Helpers.Enums.Icon;
 import Helpers.Enums.StaticText;
 import Models.Client;
-import Pages.AddressFormPage;
-import Pages.BoxmachineFormPage;
-import Pages.FormPage;
-import Pages.SummaryPage;
+import Pages.*;
 import SeleniumBase.Base;
 import org.junit.jupiter.api.*;
 
@@ -27,110 +24,183 @@ public class FlowTest {
     @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
     @Order(1)
     @DisplayName("Parcelmachine delivery method")
-    public class ParcelmachineDeliveryTest extends Base{
+    public class ParcelmachineDeliveryTest extends Base {
         private BoxmachineFormPage boxmachineFormPage = new BoxmachineFormPage();
-        private SummaryPage summaryPage = new SummaryPage("modal","parcelmachine");
+        private SummaryPage summaryPage = new SummaryPage("modal", "parcelmachine");
         private FormPage formPage = new FormPage();
+        private PaymentFormPage paymentFormPage = new PaymentFormPage();
+        private PaymentRedirectPage paymentRedirectPage = new PaymentRedirectPage();
+        private SummaryPage finalSummaryPage = new SummaryPage("final", "parcelmachine");
 
         @Test
         @Order(1)
         @DisplayName("Page loaded")
-        public void Should_LoadPage(){
-            if (!(Base.environment.getEnv() == EnvironmentType.PRODUCTION)){
+        public void Should_LoadPage() {
+            if (!(Base.environment.getEnv() == EnvironmentType.PRODUCTION)) {
                 formPage.clickPolicyButton();
             }
             formPage.clickCookieButton();
 
-            Assertions.assertDoesNotThrow(()->
+            Assertions.assertDoesNotThrow(() ->
                     formPage.<FormPage>init()
             );
         }
 
         @Test
         @Order(2)
-        public void Should_SelectDeliveryMethodBoxmachine_When_Clicked(){
+        public void Should_SelectDeliveryMethodBoxmachine_When_Clicked() {
             formPage.clickBoxmachineDeliveryMethod();
             assertEquals(formPage.srcSummaryReceiverMethodIcon(), Icon.BOXMACHINE.url);
         }
+
         @Test
         @Order(3)
-        public void Should_SelectParcelSizeB_When_Clicked(){
+        public void Should_SelectParcelSizeB_When_Clicked() {
             formPage.clickParcelSizeB();
             String text = formPage.textSummarySizeText();
             boolean result = text.contains(StaticText.SIZE_PARCEL_B.pl) || text.contains(StaticText.SIZE_PARCEL_B.en);
             assertTrue(result);
         }
+
         @Test
         @Order(4)
-        public void Should_ReceiverNameBeCorrect_When_FilledWithCorrectData(){
+        public void Should_ReceiverNameBeCorrect_When_FilledWithCorrectData() {
             formPage.fillReceiverName(receiverPm.getName());
             assertTrue(formPage.valueReceiverName().contains(receiverPm.getName()));
         }
+
         @Test
         @Order(5)
-        public void Should_ReceiverPhoneBeCorrect_When_FilledWithCorrectData(){
+        public void Should_ReceiverPhoneBeCorrect_When_FilledWithCorrectData() {
             formPage.fillReceiverPhone(receiverPm.getPhone());
-            assertTrue(formPage.valueReceiverPhone().contains("+48"+receiverPm.getPhone()));
+            assertTrue(formPage.valueReceiverPhone().contains("+48" + receiverPm.getPhone()));
         }
+
         @Test
         @Order(6)
-        public void Should_ReceiverEmailBeCorrect_When_FilledWithCorrectData(){
+        public void Should_ReceiverEmailBeCorrect_When_FilledWithCorrectData() {
             formPage.fillReceiverEmail(receiverPm.getEmail());
             assertTrue(formPage.valueReceiverEmail().contains(receiverPm.getEmail()));
         }
+
         @Test
         @Order(7)
-        public void Should_ReceiverParcelmachineBeCorrect_When_FilledWithCorrectData(){
+        public void Should_ReceiverParcelmachineBeCorrect_When_FilledWithCorrectData() {
             boxmachineFormPage.setParcelmachine(receiverPm.getParcelmachine());
             assertTrue(boxmachineFormPage.textParcelmachineFieldValue().contains(receiverPm.getParcelmachine()));
         }
 
         @Test
         @Order(8)
-        public void Should_SenderNameBeCorrect_When_FilledWithCorrectData(){
+        public void Should_SenderNameBeCorrect_When_FilledWithCorrectData() {
             formPage.fillSenderName(sender.getName());
             assertTrue(formPage.valueSenderName().contains(sender.getName()));
         }
+
         @Test
         @Order(9)
-        public void Should_SenderPhoneBeCorrect_When_FilledWithCorrectData(){
+        public void Should_SenderPhoneBeCorrect_When_FilledWithCorrectData() {
             formPage.fillSenderPhone(sender.getPhone());
-            assertTrue(formPage.valueSenderPhone().contains("+48"+sender.getPhone()));
+            assertTrue(formPage.valueSenderPhone().contains("+48" + sender.getPhone()));
         }
+
         @Test
         @Order(10)
-        public void Should_SenderEmailBeCorrect_When_FilledWithCorrectData(){
+        public void Should_SenderEmailBeCorrect_When_FilledWithCorrectData() {
             formPage.fillSenderEmail(sender.getEmail());
             assertTrue(formPage.valueSenderEmail().contains(sender.getEmail()));
         }
+
         @Test
         @Order(11)
-        public void Should_ExceptionNotThrow_When_TermsCheckboxIsClicked(){
-            assertDoesNotThrow(()->
+        public void Should_ExceptionNotThrow_When_TermsCheckboxIsClicked() {
+            assertDoesNotThrow(() ->
                     formPage.clickTermsCheckbox()
             );
         }
+
         @Test
         @Order(12)
-        public void Should_DisplaySummaryModal_When_FormIsSubmitted(){
+        public void Should_DisplaySummaryModal_When_FormIsSubmitted() {
             assertTrue(formPage.submit());
         }
+
         @Test
         @Order(13)
         public void Should_ReceiverDataBeTheSame_When_FormIsSubmitted() {
-            assertAll(()->assertEquals(receiverPm.getName(), summaryPage.textReceiverName()),
-                    ()->assertEquals(receiverPm.getPhone(), summaryPage.textReceiverPhone()),
-                    ()->assertEquals(receiverPm.getEmail(), summaryPage.textReceiverEmail())
-                    );
-        }
-        @Test
-        @Order(14)
-        public void Should_SenderDataBeTheSame_When_FormIsSubmitted() {
-            assertAll(()->assertEquals(sender.getName(), summaryPage.textSenderName()),
-                    ()->assertEquals(sender.getPhone(), summaryPage.textSenderPhone()),
-                    ()->assertEquals(sender.getEmail(), summaryPage.textSenderEmail())
+            assertAll(() -> assertEquals(receiverPm.getName(), summaryPage.textReceiverName()),
+                    () -> assertEquals(receiverPm.getPhone(), summaryPage.textReceiverPhone()),
+                    () -> assertEquals(receiverPm.getEmail(), summaryPage.textReceiverEmail())
             );
         }
+
+        @Test
+        @Order(14)
+        public void Should_ParcelmachineDataBeTheSame_When_FormIsSubmitted() {
+            assertEquals(receiverPm.getParcelmachine(), summaryPage.textReceiverParcelmachineName());
+        }
+
+        @Test
+        @Order(15)
+        public void Should_SenderDataBeTheSame_When_FormIsSubmitted() {
+            assertAll(() -> assertEquals(sender.getName(), summaryPage.textSenderName()),
+                    () -> assertEquals(sender.getPhone(), summaryPage.textSenderPhone()),
+                    () -> assertEquals(sender.getEmail(), summaryPage.textSenderEmail())
+            );
+        }
+
+        @Test
+        @Order(16)
+        public void Should_LoadPaymentPage_When_PayIsClicked() {
+            summaryPage.clickPayButton();
+            assertDoesNotThrow(() ->
+                    paymentFormPage.<PaymentFormPage>init()
+            );
+        }
+
+        @Test
+        @Order(17)
+        public void Should_RedirectToSummary_When_PaymentIsDone() {
+            paymentFormPage.fillEmailField(sender.getEmail());
+            paymentFormPage.clickMtransferPaymentButton();
+            paymentFormPage.clickDataProcessingAgreementCheckbox();
+            paymentFormPage.clickFinishButton();
+            paymentRedirectPage.<PaymentRedirectPage>init().clickConfirmedPaymentButton();
+
+            assertDoesNotThrow(() ->
+                    finalSummaryPage.<SummaryPage>init()
+            );
+        }
+
+        @Test
+        @Order(18)
+        public void Should_ReceiverDataBeTheSame_When_InFinalSummary() {
+            assertAll(() -> assertEquals(receiverPm.getName(), finalSummaryPage.textReceiverName()),
+                    () -> assertEquals(receiverPm.getPhone(), finalSummaryPage.textReceiverPhone()),
+                    () -> assertEquals(receiverPm.getEmail(), finalSummaryPage.textReceiverEmail())
+            );
+        }
+
+        @Test
+        @Order(19)
+        public void Should_SenderDataBeTheSame_When_InFinalSummary() {
+            assertAll(() -> assertEquals(sender.getName(), finalSummaryPage.textSenderName()),
+                    () -> assertEquals(sender.getPhone(), finalSummaryPage.textSenderPhone()),
+                    () -> assertEquals(sender.getEmail(), finalSummaryPage.textSenderEmail())
+            );
+        }
+        @Test
+        @Order(20)
+        public void Should_ParcelmachineDataBeTheSame_When_InFinalSummary() {
+            assertEquals(receiverPm.getParcelmachine(), finalSummaryPage.textReceiverParcelmachineName());
+        }
+
+        @Test
+        @Order(21)
+        public void Should_DownloadLabel_When_ButtonClicked() {
+            finalSummaryPage.clickDownloadLabelButton();
+        }
+
     }
 
     @Nested
