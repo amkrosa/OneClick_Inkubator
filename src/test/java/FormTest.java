bef32178dd2a672c0d5befb78fdc0f18f0231f22
Anna Krasowska <annakrasowska2@gmail.com>
2021-06-24 12:46:02 +0200

    empty test classes

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -6,32 +6,5 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class FormTest extends Base {
-    private FormPage formPage = new FormPage();
-    private WaitHelper waitHelper = new WaitHelper(new WebDriverWait(Base.driver, 3));
-
-
-    @Test
-    @DisplayName("Page loaded")
-    public void pageloaded(){
-        Assertions.assertDoesNotThrow(()->
-                waitHelper.waitUntilClickable(By.cssSelector("#parcelFormButton"))
-        );
-    }
-
-    @Nested
-    @DisplayName("Delivery method")
-    public class DeliveryMethodTest{
-
-        @Test
-        @DisplayName("Given page loaded correctly when boxmachine delivery method is clicked then form is for boxmachine delivery")
-        public void Given_PageLoaded_When_BoxmachineDeliveryClicked_Then_FormIsBoxmachineDelivery(){
-            formPage.clickBoxmachineDeliveryMethod();
-        }
-        @Test
-        @DisplayName("Given page loaded correctly when data is correct then input is valid")
-        public void Given2_PageLoaded_When_DataIsCorrect_Then_InputIsValid(){
-            formPage.clickAddressDeliveryMethod();
-        }
-    }
 
 }
