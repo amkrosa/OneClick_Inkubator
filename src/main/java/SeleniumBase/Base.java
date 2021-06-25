58c529b0ca45df85e1d9340fde79307f7b07a000
Krasowska Anna <annakrasowska2@gmail.com>
2021-06-25 11:45:06 +0200

    Selenium Base class added with default directory option for ChromeDriver and disabled prompt for download. Added Download directory path

--- src/main/java/SeleniumBase/Base.java
+++ src/main/java/SeleniumBase/Base.java
@@ -10,18 +10,32 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.html5.LocalStorage;
 import org.openqa.selenium.html5.WebStorage;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.ObjectInputFilter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Base {
 
     public static WebDriver driver;
     public static Environment environment;
     public static Config config;
+    public static final String downloadFolder = System.getProperty("user.dir")+
+            File.separator+"src"+
+            File.separator+"test"+
+            File.separator+"resources"+
+            File.separator+"Download";
 
     @BeforeAll
     public static void setUp() throws FileNotFoundException {
@@ -34,7 +48,12 @@ public class Base {
         }
 
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        Map<String, Object> prefs = new HashMap<String, Object>();
+        prefs.put("download.default_directory", downloadFolder);
+        prefs.put("download.prompt_for_download", false);
+        options.setExperimentalOption("prefs", prefs);
+        driver = new ChromeDriver(options);
         LocalStorage local = ((WebStorage) driver).getLocalStorage();
         driver.manage().window().maximize();
 
@@ -43,7 +62,23 @@ public class Base {
     }
 
     @AfterAll
-    public static void tearDown(){
+    public static void tearDown() throws IOException {
+
+        final Path targetFolder = Path.of(Base.downloadFolder);
+        List<String> result;
+        try (Stream<Path> walk = Files.walk(targetFolder)) {
+            result = walk
+                    .filter(p -> !Files.isDirectory(p))
+                    .map(p -> p.toString().toLowerCase())
+                    .filter(f -> f.endsWith("crdownload"))
+                    .collect(Collectors.toList());
+        }
+        result.forEach(e-> {
+            try {
+                Files.delete(Path.of(e));
+            } catch (IOException ignored) {}
+        });
+
         driver.quit();
     }
 }
