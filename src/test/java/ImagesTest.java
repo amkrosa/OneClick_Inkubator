bbf91029fd4211702660c6f5b9cd3f71e8a35ba9
Krasowska Anna <annakrasowska2@gmail.com>
2021-06-30 13:29:15 +0200

    Base for ImagesTest

--- src/test/java/ImagesTest.java
+++ src/test/java/ImagesTest.java
@@ -1,4 +1,20 @@
 import SeleniumBase.Base;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class ImagesTest extends Base {
+
+public class ImagesTest {
+
+    @Nested
+    public class ImagesFormPageTest extends Base{
+
+        @Test
+        public void Should_BannerBeDisplayed_When_PageIsLoaded(){
+            page.Form.init();
+            assertTrue(page.Form.isBannerImagesVisible(), "Banner is not visible");
+        }
+
+    }
+
 }
