a5ceab19cc2152d10fbaf90ff3099f4aa28fa553
Krasowska Anna <annakrasowska2@gmail.com>
2021-07-01 00:47:43 +0200

    How to pack and How to send modals tests

---
+++ src/test/java/Static/ModalTest.java
@@ -0,0 +1,48 @@
+package Static;
+
+import Helpers.Enums.Statics.StaticText;
+import Selenium.Base;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ModalTest {
+
+    @Nested
+    @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+    class HowToSendModalTest extends Base {
+
+        @Test
+        @Order(1)
+        public void Should_DisplayHowToSendModal_When_ButtonClicked() {
+            page.Form.howToSendButton().click();
+            assertTrue(page.Form.isHowToSendModalVisible());
+        }
+
+        @Test
+        @Order(2)
+        public void Should_DisplayHowToSendText_When_ModalIsDisplayed() {
+            assertTrue(page.Form.isTextFound(StaticText.MODAL_HOWTOSEND.current()));
+        }
+    }
+
+    @Nested
+    @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+    class HowToPackModalTest extends Base{
+
+        @Test
+        @Order(1)
+        public void Should_DisplayHowToPackModal_When_ButtonClicked() {
+            page.Form.howToPackButton().click();
+            assertTrue(page.Form.isHowToPackModalVisible());
+        }
+
+        @Test
+        @Order(2)
+        public void Should_DisplayHowToPackNext_When_ModalIsDisplayed() {
+            assertTrue(page.Form.isTextFound(StaticText.MODAL_HOWTOPACK.current()));
+        }
+
+    }
+
+}
