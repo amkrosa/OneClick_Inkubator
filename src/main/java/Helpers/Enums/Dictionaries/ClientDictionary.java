43eb48248663d1773c67b1d27021b32818975b78
Krasowska Anna <annakrasowska2@gmail.com>
2021-06-30 13:24:45 +0200

    ClientDictionary for storing constant Client objects

---
+++ src/main/java/Helpers/Enums/Dictionaries/ClientDictionary.java
@@ -0,0 +1,25 @@
+package Helpers.Enums.Dictionaries;
+
+import Models.Client;
+import SeleniumBase.Base;
+
+public enum ClientDictionary {
+
+    BOXMACHINE("anna krasowska", "123123123", "anna@anna.pl", "KRA01M"),
+    ADDRESS("anna krasowska", "123123123", "anna@anna.pl", "01-123", "Warszawa", "Elekcyjna", "1", "1"),
+    SENDER("jan kowalski", "321321321", "jan@jan.pl");
+
+    public final Client client;
+
+    ClientDictionary(String name, String phone, String email) {
+        client = new Client(name, phone, email);
+    }
+
+    ClientDictionary(String name, String phone, String email, String parcelmachine) {
+        client = new Client(name, phone, email, parcelmachine);
+    }
+
+    ClientDictionary(String name, String phone, String email, String zipCode, String city, String street, String buildingNo, String flatNo) {
+        client = new Client(name, phone, email, zipCode, city, street, buildingNo, flatNo);
+    }
+}
