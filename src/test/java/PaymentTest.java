0ba71c61a7c27eb5837943a7e24e6ebc12aab153
Krasowska Anna <annakrasowska2@gmail.com>
2021-06-30 13:33:26 +0200

    Navigate static methods used, refactored into using Page object, Clients are initialized with ClientDictionary values

--- src/test/java/PaymentTest.java
+++ src/test/java/PaymentTest.java
@@ -1,6 +1,7 @@
-import Configs.Environment;
-import Helpers.Enums.DeliveryMethod;
-import Helpers.Enums.StaticText;
+import Helpers.Enums.Dictionaries.ClientDictionary;
+import Helpers.Enums.Types.DeliveryMethod;
+import Helpers.Enums.Statics.StaticText;
+import Helpers.Navigate;
 import Models.Client;
 import Pages.FormPage;
 import Pages.PaymentFormPage;
@@ -9,7 +10,6 @@ import Pages.SummaryPage;
 import SeleniumBase.Base;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.function.Executable;
-import org.junit.jupiter.api.function.ThrowingSupplier;
 import org.openqa.selenium.TimeoutException;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,54 +17,45 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class PaymentTest {
 
-    private final Client receiverPm = new Client("Anna Krasowska", "123123123", "anna@anna.pl", "KRA01M");
-    private final Client sender = new Client("Magda Asowska", "321321321", "magda@magda.pl");
-    private final FormPage formPage = new FormPage();
-    private final PaymentFormPage paymentFormPage = new PaymentFormPage();
-    private final PaymentRedirectPage paymentRedirectPage = new PaymentRedirectPage();
+    private final Client receiverPm = ClientDictionary.BOXMACHINE.client;
+    private final Client sender = ClientDictionary.SENDER.client;
 
     @Nested
     @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
     @DisplayName("Payment successful")
     public class PaymentSuccessfulTest extends Base{
-        private final SummaryPage finalSummaryPage = new SummaryPage("final", DeliveryMethod.BOXMACHINE);
-
 
         @Test
         @Order(1)
         @DisplayName("Page loaded")
         public void Should_LoadPage() {
             Assertions.assertDoesNotThrow(() ->
-                    formPage.<FormPage>init()
+                    page.Form.<FormPage>init()
             );
         }
 
         @Test
         @Order(2)
         public void Should_RedirectToPaymentPage_When_FilledFormIsSubmitted(){
-            formPage.fillForm(DeliveryMethod.BOXMACHINE, receiverPm, sender);
-            formPage.submit();
-            new SummaryPage("modal", DeliveryMethod.BOXMACHINE).payButton().click();
+            Navigate.FillFormPage(DeliveryMethod.BOXMACHINE, receiverPm, sender);
+            page.Form.submit();
+            page.ModalSummary.payButton().click();
             assertDoesNotThrow(() ->
-                    paymentFormPage.<PaymentFormPage>init()
+                    page.PaymentForm.<PaymentFormPage>init()
             );
         }
         @Test
         @Order(3)
         public void Should_RedirectToFinalSummary_When_PaymentIsSuccessful(){
-            paymentFormPage
-                    .emailField().fill(sender.getEmail())
-                    .page()
-                    .mtransferPaymentButton().click()
-                    .page()
-                    .finishButton().click();
-            paymentRedirectPage.confirmedPaymentButton().click();
-            assertDoesNotThrow((Executable)finalSummaryPage::<SummaryPage>init);
+            Navigate.ThroughPaymentPage();
+            page.PaymentRedirect.confirmedPaymentButton().click();
+            page.FinalSummary.setPaymentStatus(StaticText.SUMMARY_TRANSACTION_SUCCESS);
+            assertDoesNotThrow((Executable)page.FinalSummary::<SummaryPage>init);
         }
         @Test
         @Order(4)
-        public void Should_DisplaySuccessfulPaymentText_When_InFinalSummary(){
-            assertTrue(finalSummaryPage.isTextFound(Base.config.getLanguage().equals("pl")
+        public void Should_DisplaySuccessfulPaymentText_When_InFinalSummary() {
+            assertTrue(page.FinalSummary.isTextFound(Base.config.getLanguage().equals("pl")
                     ? StaticText.SUMMARY_TRANSACTION_SUCCESS.pl
                     : StaticText.SUMMARY_TRANSACTION_SUCCESS.en));
         }
@@ -74,44 +65,29 @@ public class PaymentTest {
     @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
     @DisplayName("Payment failed")
     public class PaymentFailedTest extends Base{
-        private final SummaryPage finalSummaryPage = new SummaryPage("final", DeliveryMethod.BOXMACHINE);
-
-
-        @Test
-        @Order(1)
-        @DisplayName("Page loaded")
-        public void Should_LoadPage() {
-            Assertions.assertDoesNotThrow(() ->
-                    formPage.<FormPage>init()
-            );
-        }
 
         @Test
         @Order(2)
         public void Should_RedirectToPaymentPage_When_FilledFormIsSubmitted(){
-            formPage.fillForm(DeliveryMethod.BOXMACHINE, receiverPm, sender);
-            formPage.submit();
-            new SummaryPage("modal", DeliveryMethod.BOXMACHINE).payButton().click();
+            Navigate.FillFormPage(DeliveryMethod.BOXMACHINE, receiverPm, sender);
+            page.Form.submit();
+            page.ModalSummary.payButton().click();
             assertDoesNotThrow(() ->
-                    paymentFormPage.<PaymentFormPage>init()
+                    page.PaymentForm.<PaymentFormPage>init()
             );
         }
         @Test
         @Order(3)
         public void Should_RedirectToFinalSummary_When_PaymentIsFailed(){
-            paymentFormPage
-                    .emailField().fill(sender.getEmail())
-                    .page()
-                    .mtransferPaymentButton().click()
-                    .page()
-                    .finishButton().click();
-            paymentRedirectPage.rejectedPaymentButton().click();
-            assertDoesNotThrow((Executable)finalSummaryPage::<SummaryPage>init);
+            Navigate.ThroughPaymentPage();
+            page.PaymentRedirect.rejectedPaymentButton().click();
+            page.FinalSummary.setPaymentStatus(StaticText.SUMMARY_TRANSACTION_FAILURE);
+            assertDoesNotThrow((Executable)page.FinalSummary::<SummaryPage>init);
         }
         @Test
         @Order(4)
         public void Should_DisplayFailedPaymentText_When_InFinalSummary(){
-            assertTrue(finalSummaryPage.isTextFound(Base.config.getLanguage().equals("pl")
+            assertTrue(page.FinalSummary.isTextFound(Base.config.getLanguage().equals("pl")
                     ? StaticText.SUMMARY_TRANSACTION_FAILURE.pl
                     : StaticText.SUMMARY_TRANSACTION_FAILURE.en));
         }
@@ -121,42 +97,35 @@ public class PaymentTest {
     @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
     @DisplayName("Payment pending")
     public class PaymentPendingTest extends Base{
-        private final SummaryPage finalSummaryPage = new SummaryPage("final", DeliveryMethod.BOXMACHINE, StaticText.SUMMARY_TRANSACTION_SUCCESS);
 
         @Test
         @Order(1)
         @DisplayName("Page loaded")
         public void Should_LoadPage() {
             Assertions.assertDoesNotThrow(() ->
-                    formPage.<FormPage>init()
+                    page.Form.<FormPage>init()
             );
         }
 
         @Test
         @Order(2)
         public void Should_RedirectToPaymentPage_When_FilledFormIsSubmitted(){
-            formPage.fillForm(DeliveryMethod.BOXMACHINE, receiverPm, sender);
-            formPage.submit();
-            new SummaryPage("modal", DeliveryMethod.BOXMACHINE).payButton().click();
-            assertDoesNotThrow(() ->
-                    paymentFormPage.<PaymentFormPage>init()
-            );
+            Navigate.FillFormPage(DeliveryMethod.BOXMACHINE, receiverPm, sender);
+            page.Form.submit();
+            page.ModalSummary.payButton().click();
+            assertDoesNotThrow((Executable)page.PaymentForm::<PaymentFormPage>init);
         }
         @Test
         @Order(3)
         public void Should_RedirectToFinalSummary_When_PaymentIsPending(){
-            paymentFormPage
-                    .emailField().fill(sender.getEmail())
-                    .page()
-                    .mtransferPaymentButton().click()
-                    .page()
-                    .finishButton().click();
-            paymentRedirectPage.pendingPaymentButton().click();
+            Navigate.ThroughPaymentPage();
+            page.PaymentRedirect.pendingPaymentButton().click();
+            assertTrue(page.FinalSummary.refreshButton().waitVisible().isDisplayed());
         }
         @Test
         @Order(4)
         public void Should_ThrowTimeoutException_When_InFinalSummaryAndPendingAfterPeriodOfTime(){
-            assertThrows(TimeoutException.class,finalSummaryPage::<SummaryPage>init);
+            assertThrows(TimeoutException.class,page.FinalSummary::<SummaryPage>init);
         }
     }
 
